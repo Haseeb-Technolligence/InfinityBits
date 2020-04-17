d54d293b8d6ac35ee38998852d4121522ae15bcd
Haseeb <haxcebmalik@yahoo.com>
2020-04-18 03:48:43 +0500

    start commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const First = () =>{
+  return <h1>Hello I amm Haseeb</h1>
+}
+
+ReactDOM.render(<First />, document.querySelector('#root'));
